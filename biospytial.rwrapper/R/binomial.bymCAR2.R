541cc9deae031db327d274b36ffdfa71238c1fce
Biospytial User <molgor@gmail.com>
2019-03-21 22:47:40 +0000

    Implemented test version of model2

--- biospytial.rwrapper/R/binomial.bymCAR2.R
+++ biospytial.rwrapper/R/binomial.bymCAR2.R
@@ -273,87 +273,71 @@ CarSampler = partial(CompleteCarSampler,
 ###################################
 #### Summarise and save the results 
 ###################################
-source("summary.results.R")
 ####[J] Compute the acceptance rates
 ###[J] accept[2], accept[4] and accept[6] are the total number of iterations
-
-#accept.beta <- 100 * accept.all[1] / accept.all[2]
-#accept.phi <- 100 * accept.all[3] / accept.all[4]
-#accept.theta <- 100 * accept.all[5] / accept.all[6]
-#accept.tau2 <- 100
-#accept.sigma2 <- 100
-#accept.final <- c(accept.beta, accept.phi, accept.theta, accept.tau2, accept.sigma2)
-#names(accept.final) <- c("beta", "phi", "theta", "tau2", "sigma2")
-#
-
-#
-#
-##### Compute the fitted deviance
-#mean.beta <- apply(samples.beta, 2, mean)
-#mean.re <- apply(samples.re, 2, mean)
-#mean.logit <- as.numeric(X.standardised %*% mean.beta) + mean.re + offset    
-#mean.prob <- exp(mean.logit)  / (1 + exp(mean.logit))
-#fitted.mean <- trials * mean.prob
-#deviance.fitted <- -2 * sum(dbinom(x=Y, size=trials, prob=mean.prob, log=TRUE), na.rm=TRUE)
-#
-#
-##### Model fit criteria
-###### returns some estimators given deviance and loglikelihood
-#modelfit <- common.modelfit(samples.loglike, deviance.fitted)
-#
-#
-##### transform the parameters back to the origianl covariate scale.
-#samples.beta.orig <- common.betatransform(samples.beta, X.indicator, X.mean, X.sd, p, FALSE)
-#
-#
-##### Create a summary object
-#samples.beta.orig <- mcmc(samples.beta.orig)
-#summary.beta <- t(apply(samples.beta.orig, 2, quantile, c(0.5, 0.025, 0.975))) 
-#summary.beta <- cbind(summary.beta, rep(n.keep, p), rep(accept.beta,p), effectiveSize(samples.beta.orig), geweke.diag(samples.beta.orig)$z)
-#rownames(summary.beta) <- colnames(X)
-#colnames(summary.beta) <- c("Median", "2.5%", "97.5%", "n.sample", "% accept", "n.effective", "Geweke.diag")
-#
-##### Summary of hyperparameters
-##### tau2, sigma2, 
-#summary.hyper <- array(NA, c(2,7))
-#summary.hyper[1, 1:3] <- quantile(samples.tau2, c(0.5, 0.025, 0.975))
-#summary.hyper[1, 4:7] <- c(n.keep, accept.tau2, effectiveSize(samples.tau2), geweke.diag(samples.tau2)$z)
-#summary.hyper[2, 1:3] <- quantile(samples.sigma2, c(0.5, 0.025, 0.975))
-#summary.hyper[2, 4:7] <- c(n.keep, accept.sigma2, effectiveSize(samples.sigma2), geweke.diag(samples.sigma2)$z)
-#
-#summary.results <- rbind(summary.beta, summary.hyper)
-#rownames(summary.results)[(nrow(summary.results)-1):(nrow(summary.results))] <- c("tau2", "sigma2")
-#summary.results[ , 1:3] <- round(summary.results[ , 1:3], 4)
-#summary.results[ , 4:7] <- round(summary.results[ , 4:7], 1)
-#
-
 accept.all  <- model.sample$accept.all
-results  <- SummariseResults(samples.Y = samples.Y, samples.fitted = samples.fitted, samples.beta = samples.beta,  samples.re = samples.re, samples.tau2 = samples.tau2, samples.sigma2 =  samples.sigma2, samples.loglike = samples.loglike, X = X, X.standardised = X.standardised, X.indicator = X.indicator, X.mean = X.mean, X.sd = X.sd, Y = Y, trials = trials, p = p, offset = offset, accept.all = accept.all,formula = formula, n.miss = n.miss)
+accept.beta <- 100 * accept.all[1] / accept.all[2]
+accept.phi <- 100 * accept.all[3] / accept.all[4]
+accept.theta <- 100 * accept.all[5] / accept.all[6]
+accept.tau2 <- 100
+accept.sigma2 <- 100
+accept.final <- c(accept.beta, accept.phi, accept.theta, accept.tau2, accept.sigma2)
+names(accept.final) <- c("beta", "phi", "theta", "tau2", "sigma2")
 
-#summary.results  <- sm$results
-#mean.prob  <- sm$mean.prob
-#samples.beta.orig  <- sm$samples.beta.orig
-#modelfit  <-  sm$modelfit
-#accept.final  <- sm$accept.final
-##### Create the fitted values and residuals
-###### Re check if fitted is with logit or not
-#fitted.values <- apply(samples.fitted, 2, mean)
-#response.residuals <- as.numeric(Y) - fitted.values
-#
-#pearson.residuals <- response.residuals /sqrt(fitted.values * (1 - mean.prob))
-#residuals <- data.frame(response=response.residuals, pearson=pearson.residuals)
-#
-#
+     
+#### Compute the fitted deviance
+mean.beta <- apply(samples.beta, 2, mean)
+mean.re <- apply(samples.re, 2, mean)
+mean.logit <- as.numeric(X.standardised %*% mean.beta) + mean.re + offset    
+mean.prob <- exp(mean.logit)  / (1 + exp(mean.logit))
+fitted.mean <- trials * mean.prob
+deviance.fitted <- -2 * sum(dbinom(x=Y, size=trials, prob=mean.prob, log=TRUE), na.rm=TRUE)
+
+
+#### Model fit criteria
+##### returns some estimators given deviance and loglikelihood
+modelfit <- common.modelfit(samples.loglike, deviance.fitted)
+
+
+#### transform the parameters back to the origianl covariate scale.
+samples.beta.orig <- common.betatransform(samples.beta, X.indicator, X.mean, X.sd, p, FALSE)
+
+
+#### Create a summary object
+samples.beta.orig <- mcmc(samples.beta.orig)
+summary.beta <- t(apply(samples.beta.orig, 2, quantile, c(0.5, 0.025, 0.975))) 
+summary.beta <- cbind(summary.beta, rep(n.keep, p), rep(accept.beta,p), effectiveSize(samples.beta.orig), geweke.diag(samples.beta.orig)$z)
+rownames(summary.beta) <- colnames(X)
+colnames(summary.beta) <- c("Median", "2.5%", "97.5%", "n.sample", "% accept", "n.effective", "Geweke.diag")
+
+#### Summary of hyperparameters
+#### tau2, sigma2, 
+summary.hyper <- array(NA, c(2,7))
+summary.hyper[1, 1:3] <- quantile(samples.tau2, c(0.5, 0.025, 0.975))
+summary.hyper[1, 4:7] <- c(n.keep, accept.tau2, effectiveSize(samples.tau2), geweke.diag(samples.tau2)$z)
+summary.hyper[2, 1:3] <- quantile(samples.sigma2, c(0.5, 0.025, 0.975))
+summary.hyper[2, 4:7] <- c(n.keep, accept.sigma2, effectiveSize(samples.sigma2), geweke.diag(samples.sigma2)$z)
+
+summary.results <- rbind(summary.beta, summary.hyper)
+rownames(summary.results)[(nrow(summary.results)-1):(nrow(summary.results))] <- c("tau2", "sigma2")
+summary.results[ , 1:3] <- round(summary.results[ , 1:3], 4)
+summary.results[ , 4:7] <- round(summary.results[ , 4:7], 1)
+
+
+#### Create the fitted values and residuals
+##### Re check if fitted is with logit or not
+fitted.values <- apply(samples.fitted, 2, mean)
+response.residuals <- as.numeric(Y) - fitted.values
+pearson.residuals <- response.residuals /sqrt(fitted.values * (1 - mean.prob))
+residuals <- data.frame(response=response.residuals, pearson=pearson.residuals)
+
+
 #### Compile and return the results
-#model.string <- c("Likelihood model - Binomial (logit link function)", "\nRandom effects model - BYM CAR\n")
-#if(n.miss==0) samples.Y = NA
-#
-#samples <- list(beta=samples.beta.orig, psi=mcmc(samples.re), tau2=mcmc(samples.tau2), sigma2=mcmc(samples.sigma2), fitted=mcmc(samples.fitted), Y=mcmc(samples.Y))
-#
-#
-#results <- list(summary.results=summary.results, samples=samples, fitted.values=fitted.values, residuals=residuals, modelfit=modelfit, accept=accept.final, localised.structure=NULL,  formula=formula, model=model.string, X=X)
-#
+model.string <- c("Likelihood model - Binomial (logit link function)", "\nRandom effects model - BYM CAR\n")
+if(n.miss==0) samples.Y = NA
 
+samples <- list(beta=samples.beta.orig, psi=mcmc(samples.re), tau2=mcmc(samples.tau2), sigma2=mcmc(samples.sigma2), fitted=mcmc(samples.fitted), Y=mcmc(samples.Y))
+results <- list(summary.results=summary.results, samples=samples, fitted.values=fitted.values, residuals=residuals, modelfit=modelfit, accept=accept.final, localised.structure=NULL,  formula=formula, model=model.string, X=X)
 class(results) <- "CARBayes"
 
 
--- biospytial.rwrapper/R/sandbox.R
+++ biospytial.rwrapper/R/sandbox.R
@@ -1,31 +1,4 @@
 # This is a script for loading all the necessary data for processing 
-# i.e. it is a preprocessing script.
-# Change it appropriately
-
-#my.binomial.bymCAR <- function(formula, data=NULL, trials, W, burnin, n.sample, thin=1, prior.mean.beta=NULL, prior.var.beta=NULL, prior.tau2=NULL, prior.sigma2=NULL, MALA=TRUE, verbose=TRUE)
-
-
-#my.binomial.bymCAR(formula=formula_sample,W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE)
-
-
-#model.sample <-S.CARbym(formula=formula_sample,family="binomial",W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE)
-
-### Order of the code to be executed
-# 1. Load init_data
-# 2. Load imports.R
-# 3. Load as function my_CARbym in S.CARbym.R
-# 4. Load as function my.binomial.bymCAR in my.binomial.bymCAR.R
-# 5. Run the above line
-
-#model.sample <-my_CARbym(formula=formula_sample,family="binomial",W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE,custom_function=my.binomial.bymCAR) 
-
-
-#binomial.bymCAR <- getFromNamespace('binomial.bymCAR','CARBayes')
-#
-#model_std <- binomial.bymCAR(formula=formula_sample, data=DataFrame, trials=trials, W=M_bis, burnin=10000, n.sample=15000, thin=1, prior.mean.beta=NULL, prior.var.beta=NULL, prior.tau2=NULL, prior.sigma2=NULL, MALA=TRUE, verbose=TRUE)
-#
-#
-
 
 rm(list=ls())
 source("init_data.R")
@@ -33,29 +6,85 @@ source("init_data.R")
 source("samplerCarFunction.R")
 source("binomial.jointCARModel.R")
 
-model.sample  <- binomial.bymCAR2(formula=formula_sample, data=DataFrame, trials=trials, W=M_bis, burnin=10000, n.sample=15000, thin=1, prior.mean.beta=NULL, prior.var.beta=NULL, prior.tau2=NULL, prior.sigma2=NULL, MALA=TRUE, verbose=TRUE)
 
-
-model.pines  <- binomial.bymCAR2(formula=formula_presence, data=DataFrame, trials=trials, W=M_bis, burnin=10000, n.sample=15000, thin=1, prior.mean.beta=NULL, prior.var.beta=NULL, prior.tau2=NULL, prior.sigma2=NULL, MALA=TRUE, verbose=TRUE)
-
-
-pines.state  <-  model.pines$state
-sample.state  <- model.sample$state
-#### Init data sandboxing the carsampler
-n.sample = 5000
-
+n.sample = 10000
+burnin=10000
+postburnin = burnin +1000 
+thin = 1
+#p = 3
+#aK = 4061
+verbose = TRUE
 ### Remove and leave only the necesary
 
+
+
+### Prepare common frame for both
+
+frame.sample.results <- common.frame(formula_sample, DataFrame, "binomial")
+K.sample <- frame.sample.results$n
+p.sample <- frame.sample.results$p
+X.sample <- frame.sample.results$X
+X.standardised.sample <- frame.sample.results$X.standardised
+X.sd.sample <- frame.sample.results$X.sd
+X.mean.sample <- frame.sample.results$X.mean
+X.indicator.sample <- frame.sample.results$X.indicator 
+offset.sample <- frame.sample.results$offset
+Y.sample <- frame.sample.results$Y
+which.miss.sample <- frame.sample.results$which.miss
+n.miss.sample <- frame.sample.results$n.miss  
+Y.DA.sample <- Y.sample
+
+
+frame.presence.results <- common.frame(formula_presence, DataFrame, "binomial")
+K.presence <- frame.presence.results$n
+p.presence <- frame.presence.results$p
+X.presence <- frame.presence.results$X
+
+
+X.standardised.presence <- frame.presence.results$X.standardised
+X.sd.presence <- frame.presence.results$X.sd
+X.mean.presence <- frame.presence.results$X.mean
+X.indicator.presence <- frame.presence.results$X.indicator 
+offset.presence <- frame.presence.results$offset
+Y.presence <- frame.presence.results$Y
+which.miss.presence <- frame.presence.results$which.miss
+n.miss.presence <- frame.presence.results$n.miss  
+Y.DA.presence <- Y.presence
+
+
+
+
 #### Matrices to store samples
-n.keep <- floor((n.sample - burnin)/thin)
-samples.beta <- array(NA, c(n.keep, p))
-samples.re <- array(NA, c(n.keep, K))
+#n.keep <- floor((n.sample - burnin)/thin)
+
+n.keep <- floor((n.sample)/thin)
+samples.beta.sample <- array(NA, c(n.keep, p.sample))
+samples.beta.presence <- array(NA, c(n.keep, p.sample))
+## The same because they share the GMRF
+samples.re <- array(NA, c(n.keep, K.sample))
 samples.tau2 <- array(NA, c(n.keep, 1))
 samples.sigma2 <- array(NA, c(n.keep, 1))
-samples.loglike <- array(NA, c(n.keep, K))
-samples.fitted <- array(NA, c(n.keep, K))
-#if(n.miss>0) samples.Y <- array(NA, c(n.keep, n.miss))
 
+samples.loglike.presence <- array(NA, c(n.keep, K.presence))
+samples.fitted.presence <- array(NA, c(n.keep, K.presence))
+if(n.miss.presence>0) samples.Y.presence <- array(NA, c(n.keep, n.miss.presence))
+
+samples.loglike.sample <- array(NA, c(n.keep, K.sample))
+samples.fitted.sample <- array(NA, c(n.keep, K.sample))
+if(n.miss.sample>0) samples.Y.sample <- array(NA, c(n.keep, n.miss.sample))
+
+
+
+
+model.sample  <- binomial.bymCAR2(formula=formula_sample, name='Sample Effortmodel',data=DataFrame, trials=trials, W=M_bis, burnin=burnin, n.sample=postburnin, thin=1, prior.mean.beta=NULL, prior.var.beta=NULL, prior.tau2=NULL, prior.sigma2=NULL, MALA=TRUE, verbose=TRUE)
+
+
+model.presence  <- binomial.bymCAR2(formula=formula_presence, name='Presence model', data=DataFrame, trials=trials, W=M_bis, burnin=burnin, n.sample=postburnin, thin=1, prior.mean.beta=NULL, prior.var.beta=NULL, prior.tau2=NULL, prior.sigma2=NULL, MALA=TRUE, verbose=TRUE)
+
+
+presence.state  <-  model.presence$state
+sample.state  <- model.sample$state
+#### Init data sandboxing the carsampler
 
 
 #### Start timer
@@ -69,60 +98,63 @@ samples.fitted <- array(NA, c(n.keep, K))
     percentage.points<-round((1:100/100)*n.sample)     
     }
 
-
+CarSampler.sample <- model.sample$f_sampler 
+CarSampler.presence  <- model.presence$f_sampler
 ###[J] Trace compilation
     for(j in 1:n.sample)
     {
     ## Iteration of the CarSampler
     ## What happen with the hyperparameters ??  
-      sample.state <-  CarSampler(Y.DA = sample.state$Y.DA,
+      sample.state <-  CarSampler.sample(Y.DA = sample.state$Y.DA,
                       beta =  sample.state$beta,
-                      phi = pines.state$phi,
-                      tau2 =  pines.state$tau2,
-                      theta =  pines.state$theta,
-                      sigma2 = pines.state$sigma2,
+                      phi = presence.state$phi,
+                      tau2 =  presence.state$tau2,
+                      theta =  presence.state$theta,
+                      sigma2 = presence.state$sigma2,
                       prob = sample.state$prob,
-                      proposal.sd.theta = pines.state$proposal.sd.theta,
-                      proposal.sd.phi = pines.state$proposal.sd.phi,
+                      proposal.sd.theta = presence.state$proposal.sd.theta,
+                      proposal.sd.phi = presence.state$proposal.sd.phi,
                       proposal.sd.beta = sample.state$proposal.sd.beta,
                       accept.all = sample.state$accept.all,
                       accept = sample.state$accept,
                       iter_index = j) 
 
 
-      pines.state <-  CarSampler(Y.DA = pines.state$Y.DA,
-                      beta =  pines.state$beta,
+      presence.state <-  CarSampler.presence(Y.DA = presence.state$Y.DA,
+                      beta =  presence.state$beta,
                       phi = sample.state$phi,
                       tau2 =  sample.state$tau2,
                       theta = sample.state$theta,
                       sigma2 = sample.state$sigma2,
-                      prob = pines.state$prob,
+                      prob = presence.state$prob,
                       proposal.sd.theta = sample.state$proposal.sd.theta,
                       proposal.sd.phi = sample.state$proposal.sd.phi,
-                      proposal.sd.beta = pines.state$proposal.sd.beta,
-                      accept.all = pines.state$accept.all,
-                      accept = pines.state$accept,
+                      proposal.sd.beta = presence.state$proposal.sd.beta,
+                      accept.all = presence.state$accept.all,
+                      accept = presence.state$accept,
                       iter_index = j) 
-
-
-
-
 
 
     ###################
     ## Save the results
     ###################
-        if(j > burnin & (j-burnin)%%thin==0)
+        if(j %%thin==0)
         {
 
-        ele <- (j - burnin) / thin
-        samples.beta[ele, ] <- model.sample$beta
-        samples.re[ele, ] <- model.sample$phi + model.sample$theta
-        samples.tau2[ele, ] <- model.sample$tau2
-        samples.sigma2[ele, ] <- model.sample$sigma2
-        samples.loglike[ele, ] <- model.sample$loglike
-        samples.fitted[ele, ] <- model.sample$fitted
-            if(n.miss>0) samples.Y[ele, ] <- model.sample$Y.DA[which.miss==0]
+        ele <- (j) / thin
+        #samples.beta[ele, ] <- model.sample$beta
+        samples.beta.sample[ele, ]  <- sample.state$beta
+        samples.beta.presence[ele, ]  <- presence.state$beta
+        ## Here it's the same because they are sharing the same GMRF
+        samples.re[ele, ] <- presence.state$phi + presence.state$theta
+        samples.tau2[ele, ] <- presence.state$tau2
+        samples.sigma2[ele, ] <- presence.state$sigma2
+        samples.loglike.presence[ele, ] <- presence.state$loglike
+        samples.fitted.presence[ele, ] <- presence.state$fitted
+            if(n.miss.presence>0) samples.Y.presence[ele, ] <- presence.state$Y.DA[which.miss.presence==0]
+        samples.loglike.sample[ele, ] <- sample.state$loglike
+        samples.fitted.sample[ele, ] <- sample.state$fitted
+            if(n.miss.sample>0) samples.Y.sample[ele, ] <- sample.state$Y.DA[which.miss.sample==0]
         
                }else
         {
@@ -147,6 +179,46 @@ samples.fitted <- array(NA, c(n.keep, K))
 
 
 
+results.sample  <- SummariseResults(samples.Y = samples.Y.sample, 
+                             samples.fitted = samples.fitted.sample, 
+                             samples.beta = samples.beta.sample,  
+                             samples.re = samples.re, 
+                             samples.tau2 = samples.tau2, 
+                             samples.sigma2 =  samples.sigma2, 
+                             samples.loglike = samples.loglike.sample, 
+                             X = X.sample, 
+                             X.standardised = X.standardised.sample, 
+                             X.indicator = X.indicator.sample, 
+                             X.mean = X.mean.sample, 
+                             X.sd = X.sd.sample, 
+                             Y = Y.sample, 
+                             trials = trials, 
+                             p = p.sample, 
+                             offset = offset.sample, 
+                             accept.all = model.sample$state$accept.all,
+                             formula = formula_sample, 
+                             n.miss = n.miss.sample)
+
+print("Compiling results for presence process \n")
+results.presence  <- SummariseResults(samples.Y = samples.Y.presence, 
+                             samples.fitted = samples.fitted.presence, 
+                             samples.beta = samples.beta.presence,  
+                             samples.re = samples.re, 
+                             samples.tau2 = samples.tau2, 
+                             samples.sigma2 =  samples.sigma2, 
+                             samples.loglike = samples.loglike.presence, 
+                             X = X.presence, 
+                             X.standardised = X.standardised.presence, 
+                             X.indicator = X.indicator.presence, 
+                             X.mean = X.mean.presence, 
+                             X.sd = X.sd.presence, 
+                             Y = Y.presence, 
+                             trials = trials, 
+                             p = p.presence, 
+                             offset = offset.presence, 
+                             accept.all = model.presence$state$accept.all,
+                             formula = formula_presence, 
+                             n.miss = n.miss.presence)
 
 
 
